7df5dd0ac2dcc3d3ea2839ca5c315db76cdd59e5
Sebastian Bastidas <sbastidasr@hotmail.com>
2015-04-17 19:14:18 -0500

    Initial Commit

---
+++ SebastianBastidas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SebastianBastidas
+//
+//  Created by Sebastian Bastidas on 4/17/15.
+//  Copyright (c) 2015 sbastidasr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SebastianBastidas/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SebastianBastidas
+//
+//  Created by Sebastian Bastidas on 4/17/15.
+//  Copyright (c) 2015 sbastidasr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
