01b837d0bac1ad3a5c4f6b8d87da8bf2873fdea4
Sebastian Bastidas <sbastidasr@hotmail.com>
2015-04-19 18:07:31 -0500

    Slider+Draw Path recognition not worling, everything else ok

---
+++ SebastianBastidas/DrawViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WTMGlyphDemo
+//
+//  Created by Torin Nguyen on 5/7/12.
+//  Copyright (c) 2012 torin.nguyen@2359media.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SwipeViewController.h"
+@interface DrawViewController : SwipeViewController
+
+@end
---
+++ SebastianBastidas/DrawViewController.m
@@ -0,0 +1,78 @@
+//
+//  ViewController.m
+//  WTMGlyphDemo
+//
+//  Created by Torin Nguyen on 5/7/12.
+//  Copyright (c) 2012 torin.nguyen@2359media.com. All rights reserved.
+//
+
+#define GESTURE_SCORE_THRESHOLD         0.7f
+
+#import "DrawViewController.h"
+#import "WTMGlyphDetectorView.h"
+
+@interface DrawViewController ()
+@property (nonatomic, strong) WTMGlyphDetectorView *gestureDetectorView;
+@property (nonatomic, strong) IBOutlet UILabel *lblStatus;
+@end
+
+@implementation DrawViewController
+@synthesize gestureDetectorView;
+@synthesize lblStatus;
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+  
+  self.gestureDetectorView = [[WTMGlyphDetectorView alloc] initWithFrame:self.view.bounds];
+  self.gestureDetectorView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+  self.gestureDetectorView.delegate = self;
+  [self.gestureDetectorView loadTemplatesWithNames:@"A",@"B",@"C",@"X",@"Y",@"Z",@"N", @"T", @"W", @"V", @"circle", @"square", @"triangle",@"ruby",@"4", nil];
+  [self.view addSubview:self.gestureDetectorView];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+  [super viewDidAppear:animated];
+  
+  NSString *glyphNames = [self.gestureDetectorView getGlyphNamesString];
+  if ([glyphNames length] > 0) {
+    NSString *statusText = [NSString stringWithFormat:@"Loaded with %@ templates.\n\nStart drawing.", [self.gestureDetectorView getGlyphNamesString]];
+    self.lblStatus.text = statusText;
+  }
+}
+
+- (void)viewDidUnload
+{
+  [self.gestureDetectorView removeFromSuperview];
+  self.gestureDetectorView.delegate = nil;
+  self.gestureDetectorView = nil;
+  [super viewDidUnload];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+  return YES;
+}
+
+
+#pragma mark - Delegate
+
+- (void)wtmGlyphDetectorView:(WTMGlyphDetectorView*)theView glyphDetected:(WTMGlyph *)glyph withScore:(float)score
+{
+   if (score < GESTURE_SCORE_THRESHOLD)
+    return;
+  
+  NSString *statusString = @"";
+  
+  NSString *glyphNames = [self.gestureDetectorView getGlyphNamesString];
+  if ([glyphNames length] > 0)
+    statusString = [statusString stringByAppendingFormat:@"Loaded with %@ templates.\n\n", glyphNames];
+  
+  statusString = [statusString stringByAppendingFormat:@"Last gesture detected: %@\nScore: %.3f", glyph.name, score];
+  
+  self.lblStatus.text = statusString;
+}
+
+
+@end
