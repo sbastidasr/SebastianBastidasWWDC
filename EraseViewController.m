1e7247cab84515d11cb9163e3e11572352f73a57
Sebastian Bastidas <sbastidasr@hotmail.com>
2015-04-20 19:50:55 -0500

    Swipe+DrawVC

--- EraseViewController.m
+++ EraseViewController.m
@@ -5,9 +5,11 @@
 //  Created by Sebastian Bastidas on 4/20/15.
 //  Copyright (c) 2015 sbastidasr. All rights reserved.
 //
-
+#import <QuartzCore/QuartzCore.h>
 #import "EraseViewController.h"
 #import "EraseImageView.h"
+#import "SwipeViewController.h"
+
 @interface EraseViewController ()
 
 @end
@@ -18,14 +20,25 @@
     [super viewDidLoad];
     
    CGRect newFrame=self.view.frame;
-
+  
  //   newFrame.origin.y -= (newFrame.size.height*0.08);
     
     EraseImageView* imageView = [[EraseImageView alloc]initWithFrame:newFrame];
     imageView.foregroundImage =  [UIImage imageNamed:@"color.png"];
-    imageView.backgroundImage =  [UIImage imageNamed:@"colored.png"];
-   
+      imageView.backgroundImage =  [UIImage imageNamed:@"colored.png"];
+    imageView.alpha=0.5f;
     [self.view addSubview:imageView];
+    
+    
+    /*UIView *swipeView = [[swipeView alloc]initWithFrame:CGRectMake(0, self.view.frame.size.height-60, self.view.frame.size.width, 60.0f)];
+    swipeView.backgroundColor = [UIColor colorWithRed:0.0f green:0.0f blue:0.0f alpha:0.5f];
+    
+    swipeView.layer.borderColor = [UIColor blackColor].CGColor;
+    swipeView.layer.borderWidth = 3.0f;
+
+   
+    [self.view addSubview:swipeView];
+     */
     
 }
 
